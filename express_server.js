939014f0b680c99b311f81d90cdcf517049473e9
Aden <aden.mccarville@gmail.com>
2023-11-01 11:09:54 -0400

    Added nodemon as devDependency, morgan as dependency. Added script to start up server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const nodemon = require("nodemon");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
