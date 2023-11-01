15fe034131b66263fd251a452edc2d1bfca6ec37
Aden <aden.mccarville@gmail.com>
2023-11-01 15:18:15 -0400

    Implemented cookie-parser in order to display client's username once they log in, passed username into all views

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-// const nodemon = require("nodemon");
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser')
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -33,12 +34,12 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
+  const templateVars = { greeting: "Hello World!", username: req.cookies["username"] };
   res.render("hello_world", templateVars)
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -49,7 +50,8 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:id", (req, res) => {
@@ -63,7 +65,11 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
